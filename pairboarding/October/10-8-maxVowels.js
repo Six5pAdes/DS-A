df031489fa592608b620a32fe3b7ac2e6a54f109
Austin <hnitsua13@gmail.com>
2024-10-15 13:51:07 -0700

    10-8 complete

--- pairboarding/October/10-8-maxVowels.js
+++ pairboarding/October/10-8-maxVowels.js
@@ -7,10 +7,12 @@ Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
 function maxVowels(s, k) {
   // identify vowels
   const vowels = ["a", "e", "i", "o", "u"];
-  // initialize substring, result
+  // initialize substring, n, and result
   const sub = [];
+  let n = 0;
   let result = 0;
 
+  /*
   //   for loop for navigating the strings
   for (let i = 0; i < s.length; i++) {
     // find all vowels in strings along index; if they exist, push to substrings
@@ -20,9 +22,41 @@ function maxVowels(s, k) {
       console.log(sub);
     }
   }
-
   //   for loop that limits strings to k max
-  for (let j = k; j < s.length; j++) {}
+  for (let i = k; j < s.length; i++) {
+    //   if the first element of substring is a vowel, remove it
+    if (vowels.includes(sub.shift())) {
+      result--;
+    }
+    //   if the next element is a vowel, add it to substring
+    if (vowels.includes(s[j])) {
+      sub.push(s[j]);
+      result++;
+    }
+  }
+    */
+
+  for (let i = 0; i < k; i++) {
+    if (vowels.includes(s[i])) {
+      sub.push(true);
+      n++;
+    } else {
+      sub.push(false);
+    }
+  }
+
+  result = n;
+  for (let i = k; i < s.length; i++) {
+    let first = sub.shift();
+    sub.push(vowels.includes(s[i]));
+    if (first) {
+      n--;
+    }
+    if (vowels.includes(s[i])) {
+      n++;
+    }
+    result = Math.max(result, n);
+  }
 
   return Math.max(result);
 }
@@ -32,31 +66,3 @@ console.log(maxVowels("abciiidef", 3)); // 3
 console.log(maxVowels("aeiou", 2)); // 2
 console.log(maxVowels("leetcode", 3)); // 2
 // */
-
-/*
-    let target = ['a', 'e', 'i', 'o', 'u'];
-    let substr = [];
-    let n = 0;
-    let result = 0;
-    for (let i = 0; i < k; i++) {
-        if (target.includes(s[i])) {
-            substr.push(true)
-            n++
-        } else {
-            substr.push(false)
-        }
-    }
-    result = n;
-    for (let i = k; i < s.length; i++) {
-        let first = substr.shift();
-        substr.push(target.includes(s[i]));
-        if (first) {
-            n--;
-        };
-        if (target.includes(s[i])) {
-            n++;
-        };
-        result = Math.max(result, n);
-    }
-    return result;
-    */
