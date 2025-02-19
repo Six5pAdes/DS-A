02a070c987f74e4a943a3f895d2c9b898f2eda06
Austin <hnitsua13@gmail.com>
2025-02-19 08:44:28 -0800

    2-19

--- dailies/Feb17-21.js
+++ dailies/Feb17-21.js
@@ -83,12 +83,44 @@ var findMaximizedCapital = function (k, w, profits, capital) {
     return w;
 };
 
-const k = 2, w = 0, profits = [1, 2, 3], capital = [0, 1, 1];
-console.log(findMaximizedCapital(k, w, profits, capital)); // 4
-const k2 = 3, w2 = 0, profits2 = [1, 2, 3], capital2 = [0, 1, 2];
-console.log(findMaximizedCapital(k2, w2, profits2, capital2)); // 6
+// const k = 2, w = 0, profits = [1, 2, 3], capital = [0, 1, 1];
+// console.log(findMaximizedCapital(k, w, profits, capital)); // 4
+// const k2 = 3, w2 = 0, profits2 = [1, 2, 3], capital2 = [0, 1, 2];
+// console.log(findMaximizedCapital(k2, w2, profits2, capital2)); // 6
 
 // Wed
+/* You are given two integer arrays nums1 and nums2 sorted in non-decreasing order and an integer k.
+Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+Example 1:
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+
+Example 2:
+Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+Output: [[1,1],[1,1]]
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+*/
+
+var kSmallestPairs = function (nums1, nums2, k) {
+    const pairs = [];
+    for (const num1 of nums1) {
+        for (const num2 of nums2) {
+            pairs.push([num1, num2]);
+        }
+    }
+
+    pairs.sort((a, b) => a[0] + a[1] - b[0] - b[1]);
+
+    return pairs.slice(0, k);
+};
+
+const nums1 = [1, 7, 11], nums2 = [2, 4, 6], k = 3;
+console.log(kSmallestPairs(nums1, nums2, k)); // [[1,2],[1,4],[1,6]]
+const nums21 = [1, 1, 2], nums22 = [1, 2, 3], k2 = 2;
+console.log(kSmallestPairs(nums21, nums22, k2)); // [[1,1],[1,1]]
 
 // Thurs
 
