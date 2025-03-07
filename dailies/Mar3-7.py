0b4f8fbcd5e982ca00769e38226df3154a0bd0f2
Austin <hnitsua13@gmail.com>
2025-03-07 08:53:33 -0800

    mar7

--- dailies/Mar3-7.py
+++ dailies/Mar3-7.py
@@ -140,3 +140,38 @@ print(sqrRt(4))
 print(sqrRt(8))
 
 # fri
+'''Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Example 1:
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+Example 2:
+Input: x = 2.10000, n = 3
+Output: 9.26100
+
+Example 3:
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+'''
+
+def powXN(x, n):
+    if n < 0:
+        x = 1 / x
+        n = -n
+
+    pow = 1
+    while n:
+        if n & 1:
+            pow *= x
+        x *= x
+        n >>= 1
+
+    return pow
+
+# Time: O(log n), Space: O(1)
+
+print(powXN(2.00000, 10))
+print(powXN(2.10000, 3))
+print(powXN(2.00000, -2))
