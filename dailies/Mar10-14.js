1233b58af5feb3ff703f6d0d7a28d270bb5d15eb
Austin <hnitsua13@gmail.com>
2025-03-12 08:18:31 -0700

    mar12

--- dailies/Mar10-14.js
+++ dailies/Mar10-14.js
@@ -77,11 +77,44 @@ function climbStairs(n) {
 
 // Time: O(n), Space: O(1)
 
-console.log(climbStairs(2))
-console.log(climbStairs(3))
+// console.log(climbStairs(2))
+// console.log(climbStairs(3))
 
 // weds
+/* You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
 
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+*/
+
+function robber(nums) {
+    let lastRob = 0
+    let totalRob = 0;
+
+    for (const stole of nums) {
+        let temp = Math.max(totalRob, lastRob + stole);
+        lastRob = totalRob;
+        totalRob = temp;
+    }
+
+    return totalRob
+}
+
+// Time: O(n), Space: O(1)
+
+console.log(robber([1, 2, 3, 1]))
+console.log(robber([2, 7, 9, 3, 1]))
 // thurs
 
 // fri
