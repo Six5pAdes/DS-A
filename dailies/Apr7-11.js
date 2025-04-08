db71558436980de843bad22dd5ab1df7935de8d9
Austin <hnitsua13@gmail.com>
2025-04-08 08:39:05 -0700

    apr8

--- dailies/Apr7-11.js
+++ dailies/Apr7-11.js
@@ -19,5 +19,30 @@ function hasDuplicate(nums) {
     return false;
 };
 
-console.log(hasDuplicate([1, 2, 3, 4]))
-console.log(hasDuplicate([1, 2, 3, 3]))
+// console.log(hasDuplicate([1, 2, 3, 4]))
+// console.log(hasDuplicate([1, 2, 3, 3]))
+
+// tues
+/*Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+Input: s = "racecar", t = "carrace"
+Output: true
+
+Example 2:
+Input: s = "jar", t = "jam"
+Output: false
+*/
+
+function isAnagrams(s, t) {
+    if (s.length !== t.length) return false;
+
+    const sLetters = s.split('').sort().join('')
+    const tLetters = t.split('').sort().join('')
+
+    return sLetters === tLetters
+}
+
+console.log(isAnagrams("racecar", "carrace"))
+console.log(isAnagrams("jar", "jam"))
