da6c2d8a69ff22a588ad6207dd9efa022986c725
Austin <hnitsua13@gmail.com>
2025-04-09 08:11:47 -0700

    apr9

--- dailies/Apr7-11.js
+++ dailies/Apr7-11.js
@@ -23,7 +23,7 @@ function hasDuplicate(nums) {
 // console.log(hasDuplicate([1, 2, 3, 3]))
 
 // tues
-/*Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+/* Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
 An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
 
 Example 1:
@@ -44,5 +44,42 @@ function isAnagrams(s, t) {
     return sLetters === tLetters
 }
 
-console.log(isAnagrams("racecar", "carrace"))
-console.log(isAnagrams("jar", "jam"))
+// console.log(isAnagrams("racecar", "carrace"))
+// console.log(isAnagrams("jar", "jam"))
+
+// weds
+/* Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+Return the answer with the smaller index first.
+
+Example 1:
+Input:
+nums = [3,4,5,6], target = 7
+Output: [0,1]
+Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+Example 2:
+Input: nums = [4,5,6], target = 10
+Output: [0,2]
+
+Example 3:
+Input: nums = [5,5], target = 10
+Output: [0,1]
+*/
+
+function twoSum(nums, target) {
+    for (i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+
+    return []
+}
+
+console.log(twoSum([3, 4, 5, 6], 7))
+console.log(twoSum([4, 5, 6], 10))
+console.log(twoSum([5, 5], 10))
+console.log(twoSum([2, 4, 5, 6], 20))
