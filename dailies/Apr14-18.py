3fcd5d4d75b2e5ae267056fd0c0b9cd555edbd0b
Austin <hnitsua13@gmail.com>
2025-04-15 08:41:57 -0700

    apr15

--- dailies/Apr14-18.py
+++ dailies/Apr14-18.py
@@ -26,15 +26,48 @@ def decode(encoded):
         i = j + 1 + length
     return decoded
 
-# Time: O(), Space: O()
+# Time: O(m), Space: O(m+n)
 
-print(encode(["neet", "code", "love", "you"]))
-print(decode("4#neet4#code4#love3#you"))
+# print(encode(["neet", "code", "love", "you"]))
+# print(decode("4#neet4#code4#love3#you"))
 
-print(encode(["we", "say", ":", "yes"]))
-print(decode("2#we3#say1#:3#yes"))
+# print(encode(["we", "say", ":", "yes"]))
+# print(decode("2#we3#say1#:3#yes"))
 
 # tues
+'''Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+Each product is guaranteed to fit in a 32-bit integer.
+Follow-up: Could you solve it in O(n) time without using the division operation?
+
+Example 1:
+Input: nums = [1,2,4,6]
+Output: [48,24,12,8]
+
+Example 2:
+Input: nums = [-1,0,1,2,3]
+Output: [0,-6,0,0,0]
+'''
+
+def prodExceptSelf(nums):
+    n = len(nums)
+    output = [1] * n
+    left_product = 1
+    right_product = 1
+
+    for i in range(n):
+        output[i] *= left_product
+        left_product *= nums[i]
+
+    for i in range(n - 1, -1, -1):
+        output[i] *= right_product
+        right_product *= nums[i]
+
+    return output
+
+# Time: O(n), Space: O(1)
+
+print(prodExceptSelf([1, 2, 4, 6]))
+print(prodExceptSelf([-1, 0, 1, 2, 3]))
 
 # weds
 
