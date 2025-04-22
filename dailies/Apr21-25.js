f7ef2c00f9a9cfed6dbe1710782ede5bc51cce52
Austin <hnitsua13@gmail.com>
2025-04-22 08:39:32 -0700

    apr22

--- dailies/Apr21-25.js
+++ dailies/Apr21-25.js
@@ -13,28 +13,89 @@ The sum of 1 and 2 is 3. Since we are assuming a 1-indexed array, index1 = 1, in
 */
 
 function twoSum(numbers, target) {
-    let l = 0, r = numbers.length - 1
+    let l = 0,
+        r = numbers.length - 1;
 
     while (l < r) {
-        let curr = numbers[l] + numbers[r]
+        let curr = numbers[l] + numbers[r];
 
         if (curr > target) {
-            r--
+            r--;
         } else if (curr < target) {
-            l++
+            l++;
         } else {
-            return [l + 1, r + 1]
+            return [l + 1, r + 1];
         }
     }
 
-    return []
+    return [];
 }
 
 // Time: O(n), Space: O(1)
 
-console.log(twoSum([1, 2, 3, 4], 3))
+// console.log(twoSum([1, 2, 3, 4], 3))
 
 // tues
+/* Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+The output should not contain any duplicate triplets. You may return the output and the triplets in any order.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+
+Example 2:
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+*/
+
+function threeSum(nums) {
+    nums.sort((a, b) => a - b);
+    let res = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) break;
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let l = i + 1;
+        let r = nums.length - 1;
+
+        while (l < r) {
+            const sum = nums[i] + nums[l] + nums[r];
+
+            if (sum > 0) {
+                r--;
+            } else if (sum < 0) {
+                l++;
+            } else {
+                res.push([nums[i], nums[l], nums[r]]);
+                l++;
+                r--;
+
+                while (l < r && nums[l] === nums[l - 1]) {
+                    l++;
+                }
+            }
+        }
+    }
+    return res;
+}
+
+// Time: O(n^2), Space: O(1)
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+console.log(threeSum([0, 1, 1]));
+console.log(threeSum([0, 0, 0]));
 
 // weds
 
