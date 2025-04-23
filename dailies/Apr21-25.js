e964bbf96a764f44432882783550723b638ed83e
Austin <hnitsua13@gmail.com>
2025-04-23 09:13:42 -0700

    apr23

--- dailies/Apr21-25.js
+++ dailies/Apr21-25.js
@@ -93,11 +93,44 @@ function threeSum(nums) {
 
 // Time: O(n^2), Space: O(1)
 
-console.log(threeSum([-1, 0, 1, 2, -1, -4]));
-console.log(threeSum([0, 1, 1]));
-console.log(threeSum([0, 0, 0]));
+// console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+// console.log(threeSum([0, 1, 1]));
+// console.log(threeSum([0, 0, 0]));
 
 // weds
+/* You are given an integer array heights where heights[i] represents the height of the ith bar.
+You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+
+Example 1:
+Input: height = [1,7,2,5,4,7,3,6]
+Output: 36
+
+Example 2:
+Input: height = [2,2,2]
+Output: 4
+*/
+
+function maxArea(heights) {
+    let l = 0
+    let r = heights.length - 1
+    let res = 0;
+
+    while (l < r) {
+        const area = Math.min(heights[l], heights[r]) * (r - l)
+        res = Math.max(res, area)
+
+        if (heights[l] <= heights[r]) {
+            l++
+        } else r--
+    }
+
+    return res
+}
+
+// Time: O(n), Space: O(1)
+
+console.log(maxArea([1, 7, 2, 5, 4, 7, 3, 6]))
+console.log(maxArea([2, 2, 2]))
 
 // thurs
 
