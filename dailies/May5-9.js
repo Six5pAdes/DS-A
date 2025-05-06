f9601ba6b67c87c757970f7f0cf202807ece57cb
Austin <hnitsua13@gmail.com>
2025-05-06 08:46:05 -0700

    may6

--- dailies/May5-9.js
+++ dailies/May5-9.js
@@ -44,19 +44,78 @@ function isValid(s) {
 
 // Time: O(n), Space: O(n)
 
-console.log(isValid("[]"))
-console.log(isValid("([{}])"))
-console.log(isValid("[(])"))
+// console.log(isValid("[]"))
+// console.log(isValid("([{}])"))
+// console.log(isValid("[(])"))
 
 // tues
-/*
+/* Design a stack class that supports the push, pop, top, and getMin operations.
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+Each function should run in O(1) time.
+
+Example 1:
+Input: ["MinStack", "push", 1, "push", 2, "push", 0, "getMin", "pop", "top", "getMin"]
+Output: [null,null,null,null,0,null,2,1]
+
+Explanation:
+MinStack minStack = new MinStack();
+minStack.push(1);
+minStack.push(2);
+minStack.push(0);
+minStack.getMin(); // return 0
+minStack.pop();
+minStack.top();    // return 2
+minStack.getMin(); // return 1
 */
 
-function ____() { }
+class MinStack {
+    constructor() {
+        this.stack = []
+        this.minStack = []
+    }
 
-// Time: O(), Space: O()
+    push(val) {
+        this.stack.push(val)
 
-// console.log()
+        val = Math.min(val,
+            this.minStack.length === 0 ?
+                val :
+                this.minStack[this.minStack.length - 1]
+        )
+
+        this.minStack.push(val)
+    }
+
+    pop() {
+        this.stack.pop()
+        this.minStack.pop()
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1]
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1]
+    }
+}
+
+// Time: O(1), Space: O(n)
+
+const minStack = new MinStack();
+minStack.push(1);
+minStack.push(2);
+minStack.push(0);
+minStack.getMin(); // return 0
+minStack.pop();
+minStack.top();    // return 2
+minStack.getMin(); // return 1
+console.log(minStack)
 
 // weds
 /*
