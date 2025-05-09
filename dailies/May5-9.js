92ce5c531633385d12f013ad6601da961fb8b00f
Austin <hnitsua13@gmail.com>
2025-05-09 09:27:13 -0700

    may9

--- dailies/May5-9.js
+++ dailies/May5-9.js
@@ -189,15 +189,42 @@ function parentStr(n) {
 
 // Time: O(4^n / √(n)), Space: O(n)
 
-console.log(parentStr(1))
-console.log(parentStr(3))
+// console.log(parentStr(1))
+// console.log(parentStr(3))
 
 // fri
-/*
+/* You are given an array of integers temperatures where temperatures[i] represents the daily temperatures on the ith day.
+Return an array result where result[i] is the number of days after the ith day before a warmer temperature appears on a future day. If there is no day in the future where a warmer temperature will appear for the ith day, set result[i] to 0 instead.
+
+Example 1:
+Input: temperatures = [30,38,30,36,35,40,28]
+Output: [1,4,1,2,1,0,0]
+
+Example 2:
+Input: temperatures = [22,21,20]
+Output: [0,0,0]
 */
 
-function ____() { }
+function dailyTemp(temperatures) {
+    const res = new Array(temperatures.length).fill(0)
+    const stack = []
 
-// Time: O(), Space: O()
+    for (let i = 0; i < temperatures.length; i++) {
+        const temp = temperatures[i]
 
-// console.log()
+        while (stack.length > 0 &&
+            temp > stack[stack.length - 1][0]) {
+            const [stackT, stacked] = stack.pop()
+            res[stacked] = i - stacked
+        }
+
+        stack.push([temp, i])
+    }
+
+    return res
+}
+
+// Time: O(n), Space: O(n)
+
+console.log(dailyTemp([30, 38, 30, 36, 35, 40, 28]))
+console.log(dailyTemp([22, 21, 20]))
