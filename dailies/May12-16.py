8ad25ad57896827c1ea3aa7023fbebc444b6ea7d
Austin <hnitsua13@gmail.com>
2025-05-14 08:41:30 -0700

    may14

--- dailies/May12-16.py
+++ dailies/May12-16.py
@@ -74,15 +74,38 @@ print(histogram([1,3,7]))
 
 
 # weds
+'''You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+Your solution must run in O(logn) time.
+
+Example 1:
+Input: nums = [-1,0,2,4,6,8], target = 4
+Output: 3
+
+Example 2:
+Input: nums = [-1,0,2,4,6,8], target = 3
+Output: -1
 '''
-'''
 
-# Time: O(), Space: O()
+# Time: O(logn), Space: O(1)
 
-def ____():
-    return
+def search(nums, target):
+    l = 0
+    r = len(nums) - 1
 
-print()
+    while (l <= r):
+        m = l + ((r - l) // 2)
+
+        if (nums[m] > target):
+            r = m - 1
+        elif (nums[m] < target):
+            l = m + 1
+        else: return m
+
+    return -1
+
+print(search([-1,0,2,4,6,8], 4))
+print(search([-1,0,2,4,6,8], 3))
 
 
 # thurs
