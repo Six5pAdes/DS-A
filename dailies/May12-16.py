05cb52b2396a672b58720e6bf57375f7bcb4bd17
Austin <hnitsua13@gmail.com>
2025-05-16 09:11:36 -0700

    may16

--- dailies/May12-16.py
+++ dailies/May12-16.py
@@ -142,17 +142,46 @@ def searchMatrix(matrix, target):
 
     return False
 
-print(searchMatrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 10))
-print(searchMatrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 15))
+# print(searchMatrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 10))
+# print(searchMatrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 15))
 
 
 # fri
+import math
+'''You are given an integer array piles where piles[i] is the number of bananas in the ith pile. You are also given an integer h, which represents the number of hours you have to eat all the bananas.
+You may decide your bananas-per-hour eating rate of k. Each hour, you may choose a pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, you may finish eating the pile but you can not eat from another pile in the same hour.
+Return the minimum integer k such that you can eat all the bananas within h hours.
+
+Example 1:
+Input: piles = [1,4,3,2], h = 9
+Output: 2
+Explanation: With an eating rate of 2, you can eat the bananas in 6 hours. With an eating rate of 1, you would need 10 hours to eat all the bananas (which exceeds h=9), thus the minimum eating rate is 2.
+
+Example 2:
+Input: piles = [25,10,23,4], h = 4
+Output: 25
 '''
-'''
 
-# Time: O(), Space: O()
+# Time: O(n log m), Space: O(1)
 
-def ____():
-    return
+def minEatingSpeed(piles, h):
+    l, r = 1, max(piles)
+    res = r
 
-print()
+    while (l <= r):
+        k = (l + r) // 2
+
+        timer = 0
+        for p in piles:
+            timer += math.ceil(float(p) / k)
+
+        if timer <= h:
+            res = k
+            r = k - 1
+        else:
+            l = k + 1
+
+    return res
+
+print(minEatingSpeed([1,4,3,2], 9))
+print(minEatingSpeed([25,10,23,4], 4))
