bbe20f53c2cd138c03bdc9267947c818cf33f3b3
Austin <hnitsua13@gmail.com>
2025-05-27 08:31:06 -0700

    may27

--- dailies/May26-30.js
+++ dailies/May26-30.js
@@ -39,13 +39,47 @@ function mergeTwoLists(list1, list2) {
   return dummy.next;
 }
 
-console.log(mergeTwoLists([1, 2, 4], [1, 3, 5]));
-console.log(mergeTwoLists([], [1, 2]));
-console.log(mergeTwoLists([], []));
+// console.log(mergeTwoLists([1, 2, 4], [1, 3, 5]));
+// console.log(mergeTwoLists([], [1, 2]));
+// console.log(mergeTwoLists([], []));
 
 // Time: O(m + n), Space: O(1)
 
 // tues
+/* Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+
+There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer.
+Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+Note: index is not given to you as a parameter.
+
+Example 1:
+Input: head = [1,2,3,4], index = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+Input: head = [1,2], index = -1
+Output: false
+*/
+
+function cycling(head) {
+  let first,
+    sec = head;
+
+  while (first !== null && first.next !== null) {
+    first = first.next.next;
+    sec = sec.next;
+
+    if (first === sec) return true;
+  }
+
+  return false;
+}
+
+console.log(cycling([1, 2, 3, 4]));
+console.log(cycling([1, 2]));
+
+// Time: O(n), Space: O(1)
 
 // weds
 
