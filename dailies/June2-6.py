dabb269a1697558c44206f17d72af926afaa0959
Austin <hnitsua13@gmail.com>
2025-06-06 16:55:01 -0700

    june6

--- dailies/June2-6.py
+++ dailies/June2-6.py
@@ -201,3 +201,54 @@ print(mergeKList([[]]))
 # time: O(n * k), space: O(1)
 
 # fri
+'''You are given the head of a singly linked list head and a positive integer k.
+You must reverse the first k nodes in the linked list, and then reverse the next k nodes, and so on. If there are fewer than k nodes left, leave the nodes as they are.
+Return the modified list after reversing the nodes in each group of k.
+You are only allowed to modify the nodes' next pointers, not the values of the nodes.
+
+Example 1:
+Input: head = [1,2,3,4,5,6], k = 3
+Output: [3,2,1,6,5,4]
+
+Example 2:
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+'''
+
+class reverseNodesInKGroup:
+    def reverseK(self, head, k):
+        dummy = ListNode(0, head)
+        allPrev = dummy
+
+        while True:
+            kth = self.getKth(allPrev, k)
+            if not kth:
+                break
+            allNext = kth.next
+
+            prev, curr = kth.next, allPrev.next
+            while curr != allNext:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+
+            temp = allPrev.next
+            allPrev.next = kth
+            allPrev = temp
+
+        return dummy.next
+
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+
+        return curr
+
+
+
+# print(reverseK([1,2,3,4,5], 3))
+# print(reverseK([1,2,3,4,5,6], 3))
+
+# time: O(n), space: O(1)
