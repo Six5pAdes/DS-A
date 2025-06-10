02d2ada5bc7a26777e77aa811ba9fcf0de904d29
Austin <hnitsua13@gmail.com>
2025-06-10 08:01:53 -0700

    june10

--- dailies/June9-13.js
+++ dailies/June9-13.js
@@ -14,6 +14,14 @@ Input: root = []
 Output: []
 */
 
+class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
 function invertTree(root) {
   if (!root) return null;
 
@@ -29,13 +37,48 @@ function invertTree(root) {
   return root;
 }
 
-console.log(invertTree([1, 2, 3, 4, 5, 6, 7]));
-console.log(invertTree([3, 2, 1]));
-console.log(invertTree([]));
+// console.log(invertTree([1, 2, 3, 4, 5, 6, 7]));
+// console.log(invertTree([3, 2, 1]));
+// console.log(invertTree([]));
 
 // time: O(n), space: O(n)
 
 // tues
+/* Given the root of a binary tree, return its depth.
+The depth of a binary tree is defined as the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+Input: root = [1,2,3,null,null,4]
+Output: 3
+
+Example 2:
+Input: root = []
+Output: 0
+*/
+
+function maxDepth(root) {
+  const stack = [[root, 1]];
+  let res = 0;
+
+  while (stack.length > 0) {
+    const curr = stack.pop();
+    const node = curr[0];
+    const depth = curr[1];
+
+    if (node !== null) {
+      res = Math.max(res, depth);
+      stack.push([node.left, depth + 1]);
+      stack.push([node.right, depth + 1]);
+    }
+  }
+
+  return res;
+}
+
+console.log(maxDepth([1, 2, 3, null, null, 4]));
+console.log(maxDepth([]));
+
+// time: O(n), space: O(n)
 
 // weds
 
