075dc882d34264749639650453b5593ff604a204
Austin <hnitsua13@gmail.com>
2025-06-13 08:20:45 -0700

    june13

--- dailies/June9-13.js
+++ dailies/June9-13.js
@@ -178,10 +178,49 @@ function balance(root) {
   return true;
 }
 
-console.loge(balance([1, 2, 3, null, null, 4]));
-console.loge(balance([1, 2, 3, null, null, 4, null, 5]));
-console.loge(balance([]));
+// console.loge(balance([1, 2, 3, null, null, 4]));
+// console.loge(balance([1, 2, 3, null, null, 4, null, 5]));
+// console.loge(balance([]));
 
 // time: O(n), space: O(n)
 
 // fri
+/* Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+
+Example 1:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+Input: p = [4,7], q = [4,null,7]
+Output: false
+
+Example 3:
+Input: p = [1,2,3], q = [1,3,2]
+Output: false
+*/
+
+function isSame(p, q) {
+  const stack = [[p, q]];
+
+  while (stack.length) {
+    const [node1, node2] = stack.pop();
+
+    if (!node1 && !node2) continue;
+    if (!node1 || !node2 || node1.val !== node2.val) {
+      return false;
+    }
+
+    stack.push([node1.right, node2.right]);
+    stack.push([node1.left, node2.left]);
+  }
+
+  return true;
+}
+
+console.log(isSame([1, 2, 3], [1, 2, 3]));
+console.log(isSame([4, 7], [4, null, 7]));
+console.log(isSame([1, 2, 3], [1, 3, 2]));
+
+// time: O(n), space: O(n)
