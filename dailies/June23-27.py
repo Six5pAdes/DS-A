d5a12c5dee754303d031417d64ce6f1e0f4120b6
Austin <hnitsua13@gmail.com>
2025-06-27 08:40:26 -0700

    june27

--- dailies/June23-27.py
+++ dailies/June23-27.py
@@ -163,11 +163,57 @@ print(maxPathSum([-15,10,20,None,None,15,5,-5]))
 # time: O(n), O(n)
 
 # fri
-''''''
+'''Implement an algorithm to serialize and deserialize a binary tree.
 
-def ____():
-    return
+Serialization is the process of converting an in-memory structure into a sequence of bits so that it can be stored or sent across a network to be reconstructed later in another computer environment.
+You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure. There is no additional restriction on how your serialization/deserialization algorithm should work.
 
-# print()
+Note: The input/output format in the examples is the same as how NeetCode serializes a binary tree. You do not necessarily need to follow this format.
 
-# time: O(), O()
+Example 1:
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
+
+Example 2:
+Input: root = []
+Output: []
+'''
+
+class Codec:
+
+    # Encodes a tree to a single string.
+    def serialize(root):
+        res = []
+
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ','.join(res)
+
+
+    # Decodes your encoded data to tree.
+    def deserialize(self, data):
+        vals = data.split(',')
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        return dfs()
+
+# time: O(n), O(n)
