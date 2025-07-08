93c1331d6087922ddc7b569b9c7289d4bd2a299d
Austin <hnitsua13@gmail.com>
2025-07-08 08:26:22 -0700

    july8

--- dailies/July7-11.js
+++ dailies/July7-11.js
@@ -111,9 +111,67 @@ console.log(twitter.getNewsFeed(1)); // Should print: [10]
 */
 
 // tues
-/* */
+/* The median is the middle value in a sorted list of integers. For lists of even length, there is no middle value, so the median is the mean of the two middle values.
 
-// time: O(); space: O()
+For example:
+For arr = [1,2,3], the median is 2.
+For arr = [1,2], the median is (1 + 2) / 2 = 1.5
+
+Implement the MedianFinder class:
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far.
+Example 1:
+
+Input:
+["MedianFinder", "addNum", "1", "findMedian", "addNum", "3" "findMedian", "addNum", "2", "findMedian"]
+
+Output:
+[null, null, 1.0, null, 2.0, null, 2.0]
+
+Explanation:
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.findMedian(); // return 1.0
+medianFinder.addNum(3);    // arr = [1, 3]
+medianFinder.findMedian(); // return 2.0
+medianFinder.addNum(2);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ */
+
+// time: O(log n) (addNum()), O(1) (findMedian()); space: O(n)
+
+class MedianFinder {
+  constructor() {
+    // Max-heap for lower half, min-heap for upper half
+    this.low = new PriorityQueue((a, b) => b - a); // max-heap
+    this.high = new PriorityQueue((a, b) => a - b); // min-heap
+  }
+
+  addNum(num) {
+    this.low.enqueue(num);
+    this.high.enqueue(this.low.dequeue());
+    if (this.low.size() < this.high.size()) {
+      this.low.enqueue(this.high.dequeue());
+    }
+  }
+
+  findMedian() {
+    if (this.low.size() > this.high.size()) {
+      return this.low.front();
+    } else {
+      return (this.low.front() + this.high.front()) / 2;
+    }
+  }
+}
+
+const mf = new MedianFinder();
+mf.addNum(1);
+console.log(mf.findMedian()); // 1
+mf.addNum(3);
+console.log(mf.findMedian()); // 2
+mf.addNum(2);
+console.log(mf.findMedian()); // 2
 
 // weds
 /* */
