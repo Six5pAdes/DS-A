96c01c12405f12eb1d9cebe06b282f1303f01ee3
Austin <hnitsua13@gmail.com>
2025-07-15 09:00:52 -0700

    july15

--- dailies/July14-18.py
+++ dailies/July14-18.py
@@ -34,12 +34,50 @@ def permute(nums):
     backtrack([], [False] * len(nums))
     return res
 
-# Tests
-if __name__ == "__main__":
-    print("Test 1:", permute([1,2,3]))  # Expect 6 permutations
-    print("Test 2:", permute([7]))      # Expect [[7]]
+# print("Test 1:", permute([1,2,3]))  # Expect 6 permutations
+# print("Test 2:", permute([7]))      # Expect [[7]]
 
 # tues
+'''You are given an array nums of integers, which may contain duplicates. Return all possible subsets.
+
+The solution must not contain duplicate subsets. You may return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,1]
+
+Output: [[],[1],[1,2],[1,1],[1,2,1],[2]]
+Example 2:
+
+Input: nums = [7,7]
+
+Output: [[],[7], [7,7]]
+'''
+
+# time: O(n * (2^n)), space: O(n)
+
+def dupliSubset(nums):
+    res = []
+    nums.sort()
+
+    def backtrack(i, subset):
+        if i == len(nums):
+            res.append(subset[::])
+            return
+
+        subset.append(nums[i])
+        backtrack(i + 1, subset)
+        subset.pop()
+
+        while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            i += 1
+        backtrack(i + 1, subset)
+
+    backtrack(0, [])
+    return res
+
+print(dupliSubset([1,2,1]))
+print(dupliSubset([7,7]))
 
 # weds
 
