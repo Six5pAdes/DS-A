086185531679486f767d98febac5b2f7055c97d8
Austin <hnitsua13@gmail.com>
2025-07-16 09:06:28 -0700

    july16

--- dailies/July14-18.py
+++ dailies/July14-18.py
@@ -76,10 +76,76 @@ def dupliSubset(nums):
     backtrack(0, [])
     return res
 
-print(dupliSubset([1,2,1]))
-print(dupliSubset([7,7]))
+# print(dupliSubset([1,2,1]))
+# print(dupliSubset([7,7]))
 
 # weds
+'''Given a 2-D grid of characters board and a string word, return true if the word is present in the grid, otherwise return false.
+
+For the word to be present it must be possible to form it with a path in the board with horizontally or vertically neighboring cells. The same cell may not be used more than once in a word.
+
+Example 1:
+Input:
+board = [
+  ["A","B","C","D"],
+  ["S","A","A","T"],
+  ["A","C","A","E"]
+],
+word = "CAT"
+Output: true
+
+Example 2:
+Input:
+board = [
+  ["A","B","C","D"],
+  ["S","A","A","T"],
+  ["A","C","A","E"]
+],
+word = "BAT"
+Output: false
+'''
+
+# time: O(m * (4^n)), space: O(n)
+
+def exists(board, word):
+    rows, cols = len(board), len(board[0])
+
+    def dfs(r, c, i):
+        if i == len(word):
+            return True
+
+        if (r < 0 or c < 0 or r >= rows or c >= cols or
+            word[i] != board[r][c] or board[r][c] == '#'):
+            return False
+
+        board[r][c] = '#'
+        res = (
+            dfs(r + 1, c, i + 1) or
+            dfs(r - 1, c, i + 1) or
+            dfs(r, c + 1, i + 1) or
+            dfs(r, c - 1, i + 1)
+            )
+        board[r][c] = word[i]
+        return res
+
+    for r in range(rows):
+        for c in range(cols):
+            if dfs(r, c, 0):
+                return True
+    return False
+
+print(exists(board = [
+  ["A","B","C","D"],
+  ["S","A","A","T"],
+  ["A","C","A","E"]
+],
+word = "CAT"))
+print(exists(board = [
+  ["A","B","C","D"],
+  ["S","A","A","T"],
+  ["A","C","A","E"]
+],
+word = "BAT"))
 
 # thurs
 
