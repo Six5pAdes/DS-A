443b18583f186d7bb5207e341154755d8f5a9ca8
Austin <hnitsua13@gmail.com>
2025-07-22 09:03:23 -0700

    july22

--- dailies/July21-25.js
+++ dailies/July21-25.js
@@ -60,6 +60,85 @@ console.log(solveNQueen(4));
 console.log(solveNQueen(1));
 
 // tues
+/* A prefix tree (also known as a trie) is a tree data structure used to efficiently store and retrieve keys in a set of strings. Some applications of this data structure include auto-complete and spell checker systems.
+
+Implement the PrefixTree class:
+PrefixTree() Initializes the prefix tree object.
+void insert(String word) Inserts the string word into the prefix tree.
+boolean search(String word) Returns true if the string word is in the prefix tree (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+Example 1:
+
+Input:
+["Trie", "insert", "dog", "search", "dog", "search", "do", "startsWith", "do", "insert", "do", "search", "do"]
+
+Output:
+[null, null, true, false, true, null, true]
+
+Explanation:
+PrefixTree prefixTree = new PrefixTree();
+prefixTree.insert("dog");
+prefixTree.search("dog");    // return true
+prefixTree.search("do");     // return false
+prefixTree.startsWith("do"); // return true
+prefixTree.insert("do");
+prefixTree.search("do");     // return true
+*/
+
+// time: O(n), space: O(t)
+
+class TrieNode {
+  constructor() {
+    this.children = new Map();
+    this.endOfWord = false;
+  }
+}
+
+class PrefixTree {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let curr = this.root;
+    for (let l of word) {
+      if (!curr.children.has(l)) {
+        curr.children.set(l, new TrieNode());
+      }
+
+      curr = curr.children.get(l);
+    }
+
+    curr.endOfWord = true;
+  }
+
+  search(word) {
+    let curr = this.root;
+    for (let l of word) {
+      if (!curr.children.has(l)) {
+        return false;
+      }
+
+      curr = curr.children.get(l);
+    }
+
+    return curr.endOfWord;
+  }
+
+  startsWith(prefix) {
+    let curr = this.root;
+    for (let l of prefix) {
+      if (!curr.children.has(l)) {
+        return false;
+      }
+
+      curr = curr.children.get(l);
+    }
+
+    return true;
+  }
+}
 
 // weds
 
