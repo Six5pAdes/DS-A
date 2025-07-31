ab3709df5d29ccb76995164d00c9d3e0d8401efc
Austin <hnitsua13@gmail.com>
2025-07-31 08:22:06 -0700

    july31

--- dailies/July28-Aug1.js
+++ dailies/July28-Aug1.js
@@ -215,21 +215,102 @@ function islesAndTreasures(grid) {
   }
 }
 
-console.log(
-  islesAndTreasures([
-    [2147483647, -1, 0, 2147483647],
-    [2147483647, 2147483647, 2147483647, -1],
-    [2147483647, -1, 2147483647, -1],
-    [0, -1, 2147483647, 2147483647],
-  ])
-);
-console.log(
-  islesAndTreasures([
-    [0, -1],
-    [2147483647, 2147483647],
-  ])
-);
+// console.log(
+//   islesAndTreasures([
+//     [2147483647, -1, 0, 2147483647],
+//     [2147483647, 2147483647, 2147483647, -1],
+//     [2147483647, -1, 2147483647, -1],
+//     [0, -1, 2147483647, 2147483647],
+//   ])
+// );
+// console.log(
+//   islesAndTreasures([
+//     [0, -1],
+//     [2147483647, 2147483647],
+//   ])
+// );
 
 // thurs
+/* You are given a 2-D matrix grid. Each cell can have one of three possible values:
+0 representing an empty cell
+1 representing a fresh fruit
+2 representing a rotten fruit
+Every minute, if a fresh fruit is horizontally or vertically adjacent to a rotten fruit, then the fresh fruit also becomes rotten.
+
+Return the minimum number of minutes that must elapse until there are zero fresh fruits remaining. If this state is impossible within the grid, return -1.
+
+Example 1:
+Input: grid = [[1,1,0],[0,1,1],[0,1,2]]
+Output: 4
+
+Example 2:
+Input: grid = [[1,0,1],[0,2,0],[1,0,1]]
+Output: -1
+*/
+
+// time & space: O(m * n)
+
+function rotted(grid) {
+  const q = [];
+  let fresh = 0;
+  let time = 0;
+
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      if (grid[r][c] === 1) fresh++;
+      if (grid[r][c] === 2) q.push([r, c]);
+    }
+  }
+
+  const directions = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+
+  while (fresh > 0 && q.length > 0) {
+    const length = q.length;
+    for (let i = 0; i < length; i++) {
+      const [curR, curC] = q.shift();
+
+      for (const [dr, dc] of directions) {
+        const row = curR + dr,
+          col = curC + dc;
+
+        if (
+          row >= 0 &&
+          row < grid.length &&
+          col >= 0 &&
+          col < grid[0].length &&
+          grid[row][col] === 1
+        ) {
+          grid[row][col] = 2;
+          q.push([row, col]);
+          fresh--;
+        }
+      }
+    }
+
+    time++;
+  }
+
+  return fresh === 0 ? time : -1;
+}
+
+console.log(
+  rotted([
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 1, 2],
+  ])
+);
+console.log(
+  rotted([
+    [1, 0, 1],
+    [0, 2, 0],
+    [1, 0, 1],
+  ])
+);
 
 // fri
