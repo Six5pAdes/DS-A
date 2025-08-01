3495f99db7d58c2d8c7dfb9ff96d57f658dfaa06
Austin <hnitsua13@gmail.com>
2025-08-01 08:52:31 -0700

    aug1

--- dailies/July28-Aug1.js
+++ dailies/July28-Aug1.js
@@ -298,19 +298,101 @@ function rotted(grid) {
   return fresh === 0 ? time : -1;
 }
 
-console.log(
-  rotted([
-    [1, 1, 0],
-    [0, 1, 1],
-    [0, 1, 2],
-  ])
-);
-console.log(
-  rotted([
-    [1, 0, 1],
-    [0, 2, 0],
-    [1, 0, 1],
-  ])
-);
+// console.log(
+//   rotted([
+//     [1, 1, 0],
+//     [0, 1, 1],
+//     [0, 1, 2],
+//   ])
+// );
+// console.log(
+//   rotted([
+//     [1, 0, 1],
+//     [0, 2, 0],
+//     [1, 0, 1],
+//   ])
+// );
 
 // fri
+/* You are given a rectangular island heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+The islands borders the Pacific Ocean from the top and left sides, and borders the Atlantic Ocean from the bottom and right sides.
+Water can flow in four directions (up, down, left, or right) from a cell to a neighboring cell with height equal or lower. Water can also flow into the ocean from cells adjacent to the ocean.
+
+Find all cells where water can flow from that cell to both the Pacific and Atlantic oceans. Return it as a 2D list where each element is a list [r, c] representing the row and column of the cell. You may return the answer in any order.
+
+Example 1:
+Input: heights = [
+  [4,2,7,3,4],
+  [7,4,6,4,7],
+  [6,3,5,3,6]
+]
+Output: [[0,2],[0,4],[1,0],[1,1],[1,2],[1,3],[1,4],[2,0]]
+
+Example 2:
+Input: heights = [[1],[1]]
+Output: [[0,0],[0,1]]
+*/
+
+// time & space: O(m * n)
+
+function oceans(heights) {
+  let rows = heights.length,
+    cols = heights[0].length;
+  let pac = Array.from({ length: rows }, () => Array(cols).fill(false));
+  let atl = Array.from({ length: rows }, () => Array(cols).fill(false));
+
+  const dfs = (r, c, ocean) => {
+    ocean[r][c] = true;
+    let directs = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ];
+
+    for (const [dr, dc] of directs) {
+      let nr = r + dr,
+        nc = c + dc;
+
+      if (
+        nr >= 0 &&
+        nr < rows &&
+        nc >= 0 &&
+        nc < cols &&
+        !ocean[nr][nc] &&
+        heights[nr][nc] >= heights[r][c]
+      ) {
+        dfs(nr, nc, ocean);
+      }
+    }
+  };
+
+  for (let c = 0; c < cols; c++) {
+    dfs(0, c, pac);
+    dfs(rows - 1, c, atl);
+  }
+  for (let r = 0; r < rows; r++) {
+    dfs(r, 0, pac);
+    dfs(r, cols - 1, atl);
+  }
+
+  let res = [];
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (pac[r][c] && atl[r][c]) {
+        res.push([r, c]);
+      }
+    }
+  }
+
+  return res;
+}
+
+console.log(
+  oceans([
+    [4, 2, 7, 3, 4],
+    [7, 4, 6, 4, 7],
+    [6, 3, 5, 3, 6],
+  ])
+);
+console.log(oceans([[1], [1]]));
