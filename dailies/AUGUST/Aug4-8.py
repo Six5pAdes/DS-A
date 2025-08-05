0fdecc83df5925bf60af04473bef094515f50821
Austin <hnitsua13@gmail.com>
2025-08-05 08:56:04 -0700

    aug5

--- dailies/AUGUST/Aug4-8.py
+++ dailies/AUGUST/Aug4-8.py
@@ -64,6 +64,54 @@ def solve(board):
 # ]))
 
 # tues
+'''You are given an array prerequisites where prerequisites[i] = [a, b] indicates that you must take course b first if you want to take course a.
+
+The pair [0, 1], indicates that must take course 1 before taking course 0.
+There are a total of numCourses courses you are required to take, labeled from 0 to numCourses - 1.
+
+Return true if it is possible to finish all courses, otherwise return false.
+
+Example 1:
+Input: numCourses = 2, prerequisites = [[0,1]]
+Output: true
+Explanation: First take course 1 (no prerequisites) and then take course 0.
+
+Example 2:
+Input: numCourses = 2, prerequisites = [[0,1],[1,0]]
+Output: false
+Explanation: In order to take course 1 you must take course 0, and to take course 0 you must take course 1. So it is impossible.
+'''
+
+# time & space: O(V + E)
+
+def schedules(numCourses, prerequisites):
+   queue = {i: [] for i in range(numCourses)}
+   for course, pre in prerequisites:
+      queue[course].append(pre)
+
+   visiting = set()
+
+   def dfs(course):
+      if course in visiting:
+         return False
+      if queue[course] == []:
+         return True
+
+      visiting.add(course)
+      for pre in queue[course]:
+         if not dfs(pre):
+            return False
+      visiting.remove(course)
+      queue[course] = []
+      return True
+
+   for c in range(numCourses):
+      if not dfs(c):
+         return False
+   return True
+
+print(schedules(2, [[0,1]]))
+print(schedules(2, [[0,1],[1,0]]))
 
 # weds
 
