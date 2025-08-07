78f950de4108f7d917b11a5cbd3ac165f8b308af
Austin <hnitsua13@gmail.com>
2025-08-07 09:02:21 -0700

    aug7

--- dailies/aug/Aug4-8.py
+++ dailies/aug/Aug4-8.py
@@ -155,9 +155,55 @@ def courseOrder(numCourses, prerequisites):
 
    return output if len(output) == numCourses else []
 
-print(courseOrder(3, [[1,0]]))
-print(courseOrder(3, [[0,1],[1,2],[2,0]]))
+# print(courseOrder(3, [[1,0]]))
+# print(courseOrder(3, [[0,1],[1,2],[2,0]]))
 
 # thurs
+''' Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
 
+Example 1:
+Input:
+n = 5
+edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+Output:
+true
+
+Example 2:
+Input:
+n = 5
+edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]
+Output:
+false
+
+Note:
+You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+'''
+
+# time & space: O(V + E)
+
+def validTree(n, edges):
+   if len(edges) > (n - 1): return False
+
+   adj = [[] for _ in range(n)]
+   for u, v in edges:
+      adj[u].append(v)
+      adj[v].append(u)
+
+   visit = set()
+   def dfs(node, par):
+      if node in visit:
+         return False
+
+      visit.add(node)
+      for neigh in adj[node]:
+         if neigh == par:
+            continue
+         if not dfs(neigh, node):
+            return False
+      return True
+
+   return dfs(0, -1) and len(visit) == n
+
+print(validTree(5, [[0, 1], [0, 2], [0, 3], [1, 4]]))
+print(validTree(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]))
 # fri
