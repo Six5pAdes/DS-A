abcd70af0824414ecd5733d867ed076ae1de12d8
Austin <hnitsua13@gmail.com>
2025-08-13 09:42:28 -0700

    aug13

--- dailies/aug/Aug11-15.js
+++ dailies/aug/Aug11-15.js
@@ -153,15 +153,83 @@ function transformSeq(beginWord, endWord, wordList) {
   return 0;
 }
 
-console.log(transformSeq("cat", "sag", ["bat", "bag", "sag", "dag", "dot"]));
-console.log(transformSeq("cat", "sag", ["bat", "bag", "sat", "dag", "dot"]));
+// console.log(transformSeq("cat", "sag", ["bat", "bag", "sag", "dag", "dot"]));
+// console.log(transformSeq("cat", "sag", ["bat", "bag", "sat", "dag", "dot"]));
 
 // weds
-/* */
+/* You are given a network of n directed nodes, labeled from 1 to n. You are also given times, a list of directed edges where times[i] = (ui, vi, ti).
+ui is the source node (an integer from 1 to n)
+vi is the target node (an integer from 1 to n)
+ti is the time it takes for a signal to travel from the source to the target node (an integer greater than or equal to 0).
+You are also given an integer k, representing the node that we will send a signal from.
+Return the minimum time it takes for all of the n nodes to receive the signal. If it is impossible for all the nodes to receive the signal, return -1 instead.
 
-// time: O(), space: O()
+Example 1:
+Input: times = [[1,2,1],[2,3,1],[1,4,4],[3,4,1]], n = 4, k = 1
+Output: 3
 
-function ___() {}
+Example 2:
+Input: times = [[1,2,1],[2,3,1]], n = 3, k = 2
+Output: -1
+*/
+
+// time: O(E log V), space: O(V + E)
+
+function delay(times, n, k) {
+  const edge = new Map();
+
+  for (let i = 1; i <= n; i++) {
+    edge.set(i, []);
+  }
+
+  for (const [u, v, w] of times) {
+    edge.get(u).push([v, w]);
+  }
+
+  const minHeap = new MinPriorityQueue((entry) => entry[0]);
+  minHeap.enqueue([0, k]);
+
+  const visit = new Set();
+  let t = 0;
+
+  while (!minHeap.isEmpty()) {
+    const [w1, n1] = minHeap.dequeue();
+    if (visit.has(n1)) continue;
+    visit.add(n1);
+    t = w1;
+
+    for (const [n2, w2] of edge.get(n1)) {
+      if (!visit.has(n2)) {
+        minHeap.enqueue([w1 + w2, n2]);
+      }
+    }
+  }
+
+  return visit.size === n ? t : -1;
+}
+
+console.log(
+  delay(
+    [
+      [1, 2, 1],
+      [2, 3, 1],
+      [1, 4, 4],
+      [3, 4, 1],
+    ],
+    4,
+    1
+  )
+);
+console.log(
+  delay(
+    [
+      [1, 2, 1],
+      [2, 3, 1],
+    ],
+    3,
+    2
+  )
+);
 
 // thurs
 /* */
