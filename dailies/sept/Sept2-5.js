d031a500c33c03ffa3367d30a9a7a6f43e018c58
Austin <hnitsua13@gmail.com>
2025-09-03 08:19:55 -0700

    sept3

--- dailies/sept/Sept2-5.js
+++ dailies/sept/Sept2-5.js
@@ -1,3 +1,4 @@
+// mon
 /* You are given an integer array "coins" representing coins of different denominations (e.g. 1 dollar, 5 dollars, etc) and an integer "amount" representing a target amount of money.
 Return the fewest number of coins that you need to make up the exact target amount. If it is impossible to make up the amount, return -1.
 You may assume that you have an unlimited number of each coin.
@@ -35,6 +36,48 @@ function coinChange(coins, amount) {
   return dp[amount] > amount ? -1 : dp[amount];
 }
 
-console.log(coinChange([1, 5, 10], 12));
-console.log(coinChange([2], 3));
-console.log(coinChange([1], 0));
+// console.log(coinChange([1, 5, 10], 12));
+// console.log(coinChange([2], 3));
+// console.log(coinChange([1], 0));
+
+// tues
+/* Given an integer array nums, find a subarray that has the largest product within the array and return it.
+A subarray is a contiguous non-empty sequence of elements within an array.
+You can assume the output will fit into a 32-bit integer.
+
+Example 1:
+Input: nums = [1,2,-3,4]
+Output: 4
+
+Example 2:
+Input: nums = [-2,-1]
+Output: 2
+*/
+
+// time: O(n), space: O(1)
+
+function maxProd(nums) {
+  let res = nums[0];
+  let currMin = 1,
+    currMax = 1;
+
+  for (const num of nums) {
+    const temp = currMax * num;
+
+    currMax = Math.max(Math.max(num * currMax, num * currMin), num);
+    currMin = Math.min(Math.min(temp, num * currMin), num);
+
+    res = Math.max(res, currMax);
+  }
+
+  return res;
+}
+
+console.log(maxProd([1, 2, -3, 4]));
+console.log(maxProd([-2, -1]));
+
+// weds
+
+// thurs
+
+// fri
