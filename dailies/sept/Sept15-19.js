9e9d23610fc8be60661d43510efae821044a3aec
Austin <hnitsua13@gmail.com>
2025-09-16 09:05:43 -0700

    sept16

--- dailies/sept/Sept15-19.js
+++ dailies/sept/Sept15-19.js
@@ -38,10 +38,40 @@ function partition(nums) {
   return dp[n][target];
 }
 
-console.log(partition([1, 2, 3, 4]));
-console.log(partition([1, 2, 3, 4, 5]));
+// console.log(partition([1, 2, 3, 4]));
+// console.log(partition([1, 2, 3, 4, 5]));
 
 // tues
+/* There is an m x n grid where you are allowed to move either down or to the right at any point in time.
+Given the two integers m and n, return the number of possible unique paths that can be taken from the top-left corner of the grid (grid[0][0]) to the bottom-right corner (grid[m - 1][n - 1]).
+You may assume the output will fit in a 32-bit integer.
+
+Example 1:
+Input: m = 3, n = 6
+Output: 21
+
+Example 2:
+Input: m = 3, n = 3
+Output: 6
+*/
+
+// time & space: O(m * n)
+
+function unique(m, n) {
+  let dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+  dp[m - 1][n - 1] = 1;
+
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      dp[i][j] += dp[i + 1][j] + dp[i][j + 1];
+    }
+  }
+
+  return dp[0][0];
+}
+
+console.log(unique(3, 6));
+console.log(unique(3, 3));
 
 // weds
 
