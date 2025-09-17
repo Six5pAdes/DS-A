c941799b13ba1f2752e47712e9e404b885d1bac1
Austin <hnitsua13@gmail.com>
2025-09-17 09:03:12 -0700

    sept17

--- dailies/sept/Sept15-19.js
+++ dailies/sept/Sept15-19.js
@@ -70,10 +70,51 @@ function unique(m, n) {
   return dp[0][0];
 }
 
-console.log(unique(3, 6));
-console.log(unique(3, 3));
+// console.log(unique(3, 6));
+// console.log(unique(3, 3));
 
 // weds
+/* Given two strings text1 and text2, return the length of the longest common subsequence between the two strings if one exists, otherwise return 0.
+A subsequence is a sequence that can be derived from the given sequence by deleting some or no elements without changing the relative order of the remaining characters.
+For example, "cat" is a subsequence of "crabt".
+A common subsequence of two strings is a subsequence that exists in both strings.
+
+Example 1:
+Input: text1 = "cat", text2 = "crabt"
+Output: 3
+Explanation: The longest common subsequence is "cat" which has a length of 3.
+
+Example 2:
+Input: text1 = "abcd", text2 = "abcd"
+Output: 4
+
+Example 3:
+Input: text1 = "abcd", text2 = "efgh"
+Output: 0
+*/
+
+// time & space: O(m * n)
+
+function longestCommonSequence(text1, text2) {
+  const dp = Array(text1.length + 1)
+    .fill()
+    .map(() => Array(text2.length + 1).fill(0));
+
+  for (let i = text1.length - 1; i >= 0; i--) {
+    for (let j = text2.length - 1; j >= 0; j--) {
+      if (text1[i] === text2[j]) {
+        dp[i][j] = 1 + dp[i + 1][j + 1];
+      } else {
+        dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+      }
+    }
+  }
+
+  return dp[0][0];
+}
+
+console.log(longestCommonSequence("abcd", "abcd"));
+console.log(longestCommonSequence("abcd", "efgh"));
 
 // thurs
 
