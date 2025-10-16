ade694ac9445cf54d03f3d03c388577043d32c4a
Austin <hnitsua13@gmail.com>
2025-10-16 08:31:21 -0700

    oct16

--- dailies/oct/Oct13-17.py
+++ dailies/oct/Oct13-17.py
@@ -115,18 +115,43 @@ def eraseOverlap(intervals):
 
     return res
 
-print(eraseOverlap([[1,2],[2,4],[1,4]]))
-print(eraseOverlap([[1,2],[2,4]]))
+# print(eraseOverlap([[1,2],[2,4],[1,4]]))
+# print(eraseOverlap([[1,2],[2,4]]))
 
 # thurs
-''''''
+'''Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
 
-# time: O()
+Example 1:
+Input: intervals = [(0,30),(5,10),(15,20)]
+Output: false
+Explanation:
+(0,30) and (5,10) will conflict
+(0,30) and (15,20) will conflict
 
-def ____():
-    return
+Example 2:
+Input: intervals = [(5,8),(9,15)]
+Output: true
 
-# print()
+Note:
+(0,8),(8,10) is not considered a conflict at 8
+'''
+
+# time: O(n log n), space: O(n)
+
+def meetingRooms(intervals):
+    intervals.sort(key=lambda i: i.start)
+
+    for i in range(1, len(intervals)):
+        i1 = intervals[i - 1]
+        i2 = intervals[i]
+
+        if i1.end > i2.start:
+            return False
+
+    return True
+
+print(meetingRooms([(0,30),(5,10),(15,20)]))
+print(meetingRooms([(5,8),(9,15)]))
 
 # fri
 ''''''
