a6a1f725617b4588ad360ba7292c9d6609f46235
Austin <hnitsua13@gmail.com>
2025-10-17 08:58:15 -0700

    oct17

--- dailies/oct/Oct13-17.py
+++ dailies/oct/Oct13-17.py
@@ -150,15 +150,44 @@ def meetingRooms(intervals):
 
     return True
 
-print(meetingRooms([(0,30),(5,10),(15,20)]))
-print(meetingRooms([(5,8),(9,15)]))
+# print(meetingRooms([(0,30),(5,10),(15,20)]))
+# print(meetingRooms([(5,8),(9,15)]))
 
 # fri
-''''''
+'''Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), find the minimum number of days required to schedule all meetings without any conflicts.
+Note: (0,8),(8,10) is not considered a conflict at 8.
 
-# time: O()
+Example 1:
+Input: intervals = [(0,40),(5,10),(15,20)]
+Output: 2
+Explanation:
+day1: (0,40)
+day2: (5,10),(15,20)
 
-def ____():
-    return
+Example 2:
+Input: intervals = [(4,9)]
+Output: 1
+'''
 
-# print()
+# time: O(n log n), space: O(n)
+
+def minMeetings(intervals):
+    start = sorted([i.start for i in intervals])
+    end = sorted([i.end for i in intervals])
+    res = count = 0
+    s = e = 0
+
+    while s < len(intervals):
+        if start[s] < end[e]:
+            s += 1
+            count += 1
+        else:
+            e += 1
+            count -= 1
+
+        res = max(res, count)
+
+    return res
+
+print(minMeetings([(0,40),(5,10),(15,20)]))
+print(minMeetings([(4,9)])  )
