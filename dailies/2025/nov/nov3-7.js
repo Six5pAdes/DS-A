6fbf2c5fb2b67a421375ae9fe92bde5d98a338dc
Austin <hnitsua13@gmail.com>
2025-11-06 08:37:50 -0800

    nov6

--- dailies/2025/nov/nov3-7.js
+++ dailies/2025/nov/nov3-7.js
@@ -78,16 +78,36 @@ function reverseBits(n) {
   return res >>> 0;
 }
 
-console.log(reverseBits(0o00000000000000000000000000010101));
+// console.log(reverseBits(0o00000000000000000000000000010101));
 
 // thurs
-/* */
+/* Given an array nums containing n integers in the range [0, n] without any duplicates, return the single number in the range that is missing from nums.
+Follow-up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
 
-// time: O()
+Example 1:
+Input: nums = [1,2,3]
+Output: 0
+Explanation: Since there are 3 numbers, the range is [0,3]. The missing number is 0 since it does not appear in nums.
 
-function ____() {}
+Example 2:
+Input: nums = [0,2]
+Output: 1
+*/
 
-// console.log()
+// time: O(n), space: O(1)
+
+function missingNumber(nums) {
+  let res = nums.length;
+
+  for (let i = 0; i < nums.length; i++) {
+    res += i - nums[i];
+  }
+
+  return res;
+}
+
+console.log(missingNumber([1, 2, 3]));
+console.log(missingNumber([0, 2]));
 
 // fri
 /* */
