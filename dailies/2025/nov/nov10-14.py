866978b95c5430a3b4ea0092f740dab7e295b774
Austin <hnitsua13@gmail.com>
2025-11-11 07:50:36 -0800

    nov11

--- dailies/2025/nov/nov10-14.py
+++ dailies/2025/nov/nov10-14.py
@@ -25,19 +25,44 @@ def longestCommonPrefix(strs):
 
     return strs[0]
 
-print(longestCommonPrefix(["bat","bag","bank","band"]))
-print(longestCommonPrefix(["dance","dag","danger","damage"]))
-print(longestCommonPrefix(["neet","feet"]))
+# print(longestCommonPrefix(["bat","bag","bank","band"]))
+# print(longestCommonPrefix(["dance","dag","danger","damage"]))
+# print(longestCommonPrefix(["neet","feet"]))
 
 # tues
-''''''
+'''You are given an integer array nums and an integer val. Your task is to remove all occurrences of val from nums in-place.
+After removing all occurrences of val, return the number of remaining elements, say k, such that the first k elements of nums do not contain val.
+
+Note:
+    The order of the elements which are not equal to val does not matter.
+    It is not necessary to consider elements beyond the first k positions of the array.
+    To be accepted, the first k elements of nums must contain only elements not equal to val.
+Return k as the final result.
+
+Example 1:
+Input: nums = [1,1,2,3,4], val = 1
+Output: [2,3,4]
+Explanation: You should return k = 3 as we have 3 elements which are not equal to val = 1.
+
+Example 2:
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: [0,1,3,0,4]
+Explanation: You should return k = 5 as we have 5 elements which are not equal to val = 2.
+'''
 
 # time: O()
 
-def ____():
-    return
+def removeElement(nums, val):
+    k = 0
 
-# print()
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+    return k
+
+print(removeElement([1,1,2,3,4], 1))
+print(removeElement([0,1,2,2,3,0,4,2], 2))
 
 # weds
 ''''''
