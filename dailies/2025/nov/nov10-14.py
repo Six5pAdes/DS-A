8a572ec2497bd280a926e4a881cabaf63c28137e
Austin <hnitsua13@gmail.com>
2025-11-13 08:31:34 -0800

    nov13

--- dailies/2025/nov/nov10-14.py
+++ dailies/2025/nov/nov10-14.py
@@ -90,18 +90,62 @@ def majority(nums):
 
     return res
 
-print(majority([5,5,1,1,1,5,5]))
-print(majority([2,2,2]))
+# print(majority([5,5,1,1,1,5,5]))
+# print(majority([2,2,2]))
 
 # thurs
-''''''
+'''Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+Example 1:
+Input: ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output: [null, null, null, true, false, null, true, null, false]
+
+Explanation:
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1); // set = [1]
+myHashSet.add(2); // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2); // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2); // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+'''
 
 # time: O()
 
-def ____():
-    return
+class MyHashSet:
+    def __init__(self):
+        self.set = [0] * 31251
 
-# print()
+    def add(self, key: int) -> None:
+        self.set[key // 32] |= self.getMask(key)
+
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            self.set[key // 32] ^= self.getMask(key)
+
+    def container(self, key: int) -> bool:
+        return self.set[key // 32] & self.getMask(key) != 0
+
+    def getMask(self, key: int) -> int:
+        return 1 << (key % 32)
+
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1); # set = [1]
+myHashSet.add(2); # set = [1, 2]
+myHashSet.contains(1); # return True
+myHashSet.contains(3); # return False, (not found)
+myHashSet.add(2); # set = [1, 2]
+myHashSet.contains(2); # return True
+myHashSet.remove(2); # set = [1]
+myHashSet.contains(2); # return False, (already removed)
 
 # fri
 ''''''
