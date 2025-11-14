43ce35a4865fbd683e4dbd7b4aee9129b03d7c8f
Austin <hnitsua13@gmail.com>
2025-11-14 08:53:38 -0800

    nov14

--- dailies/2025/nov/nov10-14.py
+++ dailies/2025/nov/nov10-14.py
@@ -131,13 +131,13 @@ class MyHashSet:
         if self.contains(key):
             self.set[key // 32] ^= self.getMask(key)
 
-    def container(self, key: int) -> bool:
+    def contains(self, key: int) -> bool:
         return self.set[key // 32] & self.getMask(key) != 0
 
     def getMask(self, key: int) -> int:
         return 1 << (key % 32)
 
-MyHashSet myHashSet = new MyHashSet();
+myHashSet = MyHashSet();
 myHashSet.add(1); # set = [1]
 myHashSet.add(2); # set = [1, 2]
 myHashSet.contains(1); # return True
@@ -148,11 +148,52 @@ myHashSet.remove(2); # set = [1]
 myHashSet.contains(2); # return False, (already removed)
 
 # fri
-''''''
+'''Design a HashMap without using any built-in hash table libraries.
 
-# time: O()
+Implement the MyHashMap class:
+MyHashMap() initializes the object with an empty map.
+void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
 
-def ____():
-    return
+Example 1:
+Input: ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+Output: [null, null, null, 1, -1, null, 1, null, -1]
 
-# print()
+Explanation:
+MyHashMap myHashMap = new MyHashMap();
+myHashMap.put(1, 1); // The map is now [[1,1]]
+myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+myHashMap.get(1); // return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3); // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2); // return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2); // return -1 (i.e., not found), The map is now [[1,1]]
+'''
+
+# time: O(1), space: O(1000000)
+
+class MyHashMap:
+    def __init__(self):
+        self.map = [-1] *1000001
+
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+
+    def get(self, key: int) -> int:
+        return self.map[key]
+
+    def remove(self, key: int) -> None:
+        self.map[key] = -1
+
+myHashMap = MyHashMap()
+myHashMap.put(1, 1); # The map is now [[1,1]]
+myHashMap.put(2, 2); # The map is now [[1,1], [2,2]]
+myHashMap.get(1); # return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3); # return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); # The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2); # return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); # remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2); # return -1 (i.e., not found), The map is now [[1,1]]
